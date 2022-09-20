418b75c504fe7e111e95bf40bab2459accb1d1e3
ThisOneGuy <tahir.arslan@gmail.com>
2022-09-20 17:13:45 -0400

    display all questions, highlight correct/incorrect

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,23 +1,54 @@
 // display questions
-const questions = [
+const questionsListArr = 
+[
     {
         q: "What is the answer?",
-        options: ["no", "no", "no", "yes"],
+        options: {
+            A: "No",
+            B: "No",
+            C: "No:",
+            D: "Yes"
+        },
         answer: 4
-    }, 
+    },
     {
         q: "Will the question answer?",
-        options: ["no", "yes", "no", "no"],
+        options: {
+            A: "No",
+            B: "Yes",
+            C: "No",
+            D: "No"
+        },
         answer: 2
-    }, 
+    },
     {
         q: "This is another question?",
-        options: ["yes", "no"],
+        options: {
+            A: "Yes",
+            B: "No"
+        },
         answer: 1
-    }, 
+    },
     {
         q: "How are the questions?",
-        options: ["no", "no", "yes", "no"],
-        answer: 3
+        options: {
+            A: "No",
+            B: "Tricked You",
+            C: "Yes:"
+        },
+        answer: 2
+    },
+    {
+        q: "If a question could quest questions, how many quests would a question quest?",
+        options: {
+            A: "No",
+            B: "No",
+            C: "No",
+            D: "No",
+            E: "Yes",
+            F: "No",
+            G: "No"
+        },
+        answer: 5
     }
 ];
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,127 +1,93 @@
-var startQuizBtnEl = document.getElementById("start-btn");
-var quizContainerEl = document.querySelector("#quiz");
+// complete question list already set in './js/questions.js'
+// define button elements
+var startQuizEl = document.getElementById("start-btn");
+var nextBtnEl = document.getElementById("next-btn");
 
-// start quiz
+// define screen to display
+var introScreen = document.getElementById("intro-screen");
+var questionsScreen = document.getElementById("display-questions");
+var resultsScreen = document.getElementById("result-container");
+
+// define containers to hold questions and choices
+// var availableQuestionsArr = [];
+var questionDisplayEl = document.getElementById("question-container");
+var choiceDisplayEl = document.getElementById("choice-container");
+
+let questionCount = 0;
+let choiceCount = 1;
+
 var startQuizHandler = function() {
-    document.getElementById("intro-screen").style.display = "none";
-    quizContainerEl2.style.display = "block";
-    clearInterval(timerHandler);
-    timerHandler();
-    questionDisplay();
-};
+    // change screens
+    introScreen.style.display = "none";
+    questionsScreen.style.display = "block";
+    // display question and choices
+    questionDisplayHandler();
+}
 
-// start timer countdown
-var timerEl = document.getElementById("timer");
-var countdown = 5;
-var timerHandler = function () {
-    setInterval(function () {
-        countdown --;
-        timerEl.innerHTML = "Time remaining: " + countdown;
-        if (countdown <= 0) {
-            clearInterval(timerHandler);
-            tryAgain();
-        }
-    }, 1000);
-};
+var questionDisplayHandler = function() {
+    // display question one at a time
+    questionDisplayEl.textContent = questionsListArr[questionCount].q;
 
-// reset quiz
-var tryAgain = function () {
-    countdown = 0;
-    timerEl.innerHTML = "Quiz is done, put your mouses away!";
-    var tryAgainEl = document.createElement("div");
-    tryAgainEl.setAttribute("data-screen-id", "try-again");
-};
-
-var questionText = document.getElementById("question"); //questionNumber
-var choiceContainer = document.getElementById("choice-container"); //optionContainer
-
-var questionCounter = 0;
-let currentQuestion;
-var availableQuestions = [];
-var availableOptions = [];
-
-// push remaining questions into availableQuestions array
-var setAvailableQuestions = function () {
-    const totalQuestions = questions.length; // questions = quiz
-    for (let i = 0; i < totalQuestions; i++) {
-        availableQuestions.push(questions[i]); 
-    };
-};
-
-// set question, question number, and options
-var getNewQuestion = function () {
-    // set question text and get random question
-    var questionIndex = availableQuestions[Math.floor(Math.random() * availableQuestions.length)];
-    currentQuestion = questionIndex;
-    questionText.innerHTML = currentQuestion.q;
-    // get position of 'questionIndex' from availableQuestion array
-    var index1 = availableQuestions.indexOf(questionIndex);
-    // remove questionIndex from availableQuestion Array so no repeating questions
-    availableQuestions.splice(index1, 1);
-
-    // set options and get length of options
-    var optionLength = currentQuestion.options.length;
-    // push options into availableOptions array
-    for (let i = 0; i < optionLength; i++) {
-        availableOptions.push(i);
-    };
-    // create options in innerHTML
-    for (let i = 0; i < optionLength; i++) {
-        // random option
-        var optionIndex = availableOptions[Math.floor(Math.random() * availableOptions.length)];
-        // get position of optionIndex from availableOptions
-        var index2 = availableOptions.indexOf(optionIndex);
-        // remove the optionIndex from availableOptions, so options do not repeat 
-        availableOptions.splice(index2,1);
-        
-        var option = document.createElement("div");
-        option.innerHTML = currentQuestion.options[optionIndex];
-        option.id = optionIndex;
-        option.className = "option";
-        choiceContainer.appendChild(option);
-        option.setAttribute("onclick", "getResult(this)");
-    };
-    questionCounter++;
-};
-
-// get result of current attempt
-var getResult = function (element) {
-    var id = parseInt(element.id);
-    console.log(element.id);
-    // get answer by comparing id of clicked option
-    if(id === currentQuestion.answer) {
-        element.classList.add("correct");
-    } else {
-        console.log("answer is wrong");
+    // display all options for current question
+    for (const [key, value] of Object.entries(questionsListArr[questionCount].options)) {
+        // creating <li id="choice-btn" choiceId="choiceCount"> element
+        var choice = document.createElement("li");
+        choice.className = "choice-btn"
+        choice.setAttribute("choiceId", choiceCount);
+        choice.innerHTML = key + ": " + value;
+        choiceDisplayEl.appendChild(choice);
+        choiceCount ++;
+        console.log(choice);
     }
 }
 
-var next = function () {
-    if (questionCounter === questions.length) {
-        console.log("quiz over");
-    } else {
-        getNewQuestion();
+// validate choice selection
+choiceDisplayEl.onclick = function(event) {
+    // get and store dynamically generated choiceId
+    var getChoiceId = event.target.getAttribute("choiceId");
+    // validate choice based on getChoiceId
+    if (!getChoiceId) { return };
+    // if correct, block mouseclick and highlight correct option green
+    if (getChoiceId == questionsListArr[questionCount].answer) {
+        event.target.classList.add("choice-right");
+        // choiceId..querySelector('[choiceId='choiceCount']').className = "choice-right";
+        choiceDisplayEl.style.pointerEvents = "none";
+        console.log("good stuff")
+    }
+    else {
+        // if wrong, highlight wrong option red
+        event.target.classList.add("choice-wrong");
+        console.log("clicked a choice " + getChoiceId);
+        console.log(questionsListArr[questionCount].answer);
     }
 }
 
-var questionDisplay = function () {
-    
+// listen to button click for next question
+nextBtnEl.onclick = function() {
+    // reset elements on button click for next question
+    choiceDisplayEl.innerHTML = "";
+    choiceCount = 1;
+    choiceDisplayEl.style.pointerEvents = "auto";
+    questionCount++;
+    // check if all questions are done. if yes, move on to results
+    if (questionCount === questionsListArr.length) {
+        resultsDisplayHandler();
+    }
+    else {
+        questionDisplayHandler(questionCount);
+    }
 }
 
-var resultContainerEl = document.getElementById("result-container");
-var quizContainerEl2 = document.getElementById("display-questions");
-window.onload = function () {
-    setAvailableQuestions();
-    getNewQuestion();
-    resultContainerEl.style.display = "none";
-    quizContainerEl2.style.display = "none";
-};
+var resultsDisplayHandler = function() {
+    questionsScreen.style.display = "none";
+    resultsScreen.style.display = "block";
+    console.log("quiz is complete");
+}
 
-// introScreenHandler();
-startQuizBtnEl.addEventListener("click", startQuizHandler);
+window.onload = function() {
+    let introTextEl = document.querySelector("#instructions");
+    console.log(introTextEl);
+    introTextEl.innerHTML = "There are " + questionsListArr.length + " questions in total. Try to answer the following code-related questions within the time limit. Are you up for the challenge? <br /> (Keep in mind that incorrect answers will penalize your time by ten seconds!)"
+}
 
-// // clear screen
-// var clearScreenHandler = function () {
-//     var clearScreen = quizContainerEl.getAttribute("data-screen-id");
-//     quizContainerEl.remove(clearScreen);
-// };
+startQuizEl.addEventListener("click", startQuizHandler);
